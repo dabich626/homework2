21596190b54d795ffdeffdce75b6c5477e228ed3
dabi <dabich626626@gmail.com>
2023-04-17 17:06:25 +0200

    homework2

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,49 @@ public class Main {
         task3();
         task4();
         task5();
+        task6();
+        task7();
+        task8();
+    }
+
+    private static void task8() {
+        var totalHourse = 640;
+        var workinghourseForOne = 8;
+        var workersAmount = totalHourse / workinghourseForOne;
+        System.out.println("колличество работников в компании составляет" + workersAmount);
+        var newWorkersAmount = workersAmount + 94;
+        var newWorkingHourse = workinghourseForOne * newWorkersAmount;
+        System.out.println("если в компании работает" + newWorkersAmount + "человек, то всего" + newWorkingHourse + "часов работы может быть поделено между сотрудниками");
+
+
+    }
+
+    private static void task7() {
+        var boxerWeight = 78.2;
+        var boxerWeight2 = 82.7;
+        var weightDifference = boxerWeight2 - boxerWeight;
+        System.out.println(weightDifference);
+        var weightDifference2 = boxerWeight2 % boxerWeight;
+        System.out.println("разница веса боксеров составляет" + weightDifference2);
+
+    }
+
+    private static void task6() {
+        var boxerWeight = 78.2;
+        var boxerWeight2 = 82.7;
+        var totalweight = boxerWeight + boxerWeight2;
+        System.out.println(totalweight);
+        var weightDifference = boxerWeight2 - boxerWeight;
+        System.out.println(weightDifference);
     }
 
     private static void task5() {
         var frog = 3.5;
                 frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog +4;
         System.out.println(frog);
     }
 
